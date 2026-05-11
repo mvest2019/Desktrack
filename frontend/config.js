cef18c0d3ac28f833b45085b150d1efb85fc10a7
Sanskriti <manjushakarpe1982@gmail.com>
2026-05-11 15:51:51 +0530

    env

--- frontend/config.js
+++ frontend/config.js
@@ -5,6 +5,6 @@
 //   LAN:         "http://192.168.1.77:8000"
 //   Production:  "http://108.181.168.43:8000"
 //
-const API = process.env.NEXT_PUBLIC_API_URL || "http://192.168.1.77:8000";
+const API = process.env.NEXT_PUBLIC_API_URL || "http://69.62.76.202:8000";
 
 export default API;
